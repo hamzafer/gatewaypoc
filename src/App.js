ff38172eed2580bf03feae996dbc7e7ed35aa0c5
Hamza Zafar <45764331+hamzafer@users.noreply.github.com>
2022-04-28 23:49:16 +0500

    Removed Rendered Raw HTML

--- src/App.js
+++ src/App.js
@@ -1,20 +1,4 @@
 import './App.css';
-import createDOMPurify from 'dompurify'
-import {JSDOM} from 'jsdom'
-
-const window = (new JSDOM('')).window
-const DOMPurify = createDOMPurify(window)
-
-const rawHTML = `
-    <div>Please enter your payment details:</div>
-    <h3>Credit Card</h3>
-    <div>Card Number: <input type="text" id="card-number" class="input-field" title="card number" aria-label="enter your card number" value="" tabindex="1" readonly></div>
-    <div>Expiry Month:<input type="text" id="expiry-month" class="input-field" title="expiry month" aria-label="two digit expiry month" value="" tabindex="2" readonly></div>
-    <div>Expiry Year:<input type="text" id="expiry-year" class="input-field" title="expiry year" aria-label="two digit expiry year" value="" tabindex="3" readonly></div>
-    <div>Security Code:<input type="text" id="security-code" class="input-field" title="security code" aria-label="three digit CCV security code" value="" tabindex="4" readonly></div>
-    <div>Cardholder Name:<input type="text" id="cardholder-name" class="input-field" title="cardholder name" aria-label="enter name on card" value="" tabindex="5" readonly></div>
-    <div><button id="payButton" onclick="pay('card');">Pay Now</button></div>
-`
 
 async function createSession()
 {
@@ -81,7 +65,6 @@ function App()
 {
     return (
         <>
-            {<div dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(rawHTML)}}/>}
             <button id="generate-btn" onClick={updateSession}>Session
             </button>
         </>
